fe6a12d6c97fc12a033e2e8c93199fbe8b1daa6b
yeojongki <kk597@sina.com>
2019-03-01 22:47:34 +0800

    update: public resource path

--- antd-year-picker/build/service-worker.js
+++ antd-year-picker/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d4011b75b29a719afb25017139a61f8.js"
+  "./precache-manifest.3053575ad537f42278f1d7102835796a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

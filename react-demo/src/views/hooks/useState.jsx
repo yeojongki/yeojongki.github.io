c17ec1410f2a2af377421707d14707a7083011f3
yeojongki <kk597@sina.com>
2019-06-11 23:50:05 +0800

    update: useState进一步说明

--- react-demo/src/views/hooks/useState.jsx
+++ react-demo/src/views/hooks/useState.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
 
 export default function Counter() {
+  // 每一次渲染都是独立的闭包
+  // 都有自己的Props & state
+  // logCount 会捕获点击 `logCount` 按钮时的状态
+  // 组件每次渲染都会被调用
   const [count, setCount] = useState(0)
+  function logCount() {
+    setTimeout(() => {
+      console.log(count)
+    }, 2000)
+  }
 
   return (
     <div>
@@ -13,13 +22,31 @@ export default function Counter() {
       >
         +
       </button>
-      <button
-        onClick={() => {
-          setCount(count - 1)
-        }}
-      >
-        -
-      </button>
+      <button onClick={logCount}>logCount</button>
     </div>
   )
 }
+
+// export default function Counter() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1)
+//         }}
+//       >
+//         +
+//       </button>
+//       <button
+//         onClick={() => {
+//           setCount(count - 1)
+//         }}
+//       >
+//         -
+//       </button>
+//     </div>
+//   )
+// }

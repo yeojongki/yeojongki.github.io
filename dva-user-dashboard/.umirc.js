9d0e83f1f9f9e73d4cc911c01dd354b43c7e271a
yeojongki <kk597@sina.com>
2019-06-25 19:59:07 +0800

    feat: dva demo

---
+++ dva-user-dashboard/.umirc.js
@@ -0,0 +1,11 @@
+export default {
+  plugins: ['umi-plugin-dva'],
+  proxy: {
+    '/api': {
+      target: 'http://jsonplaceholder.typicode.com/',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' }
+    }
+  },
+  publicPath: '.'
+}

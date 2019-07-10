0a5cd76ded27918591f077a8efc2a3bd48057fa3
yeojongki <kk597@sina.com>
2019-07-11 01:01:25 +0800

    feat: single linkList

--- algorithm/linkList/linkList.js
+++ algorithm/linkList/linkList.js
@@ -1,11 +1,15 @@
 const LinkListNode = require('./linkListNode')
 
-class LinkList {
+class SingleLinkList {
   constructor() {
     this.head = null
     this.length = 0
   }
 
+  /**
+   * 添加元素到链表尾部
+   * @param {*} value
+   */
   append(value) {
     const newNode = new LinkListNode(value)
     // 如果不存在链表首位
@@ -15,9 +19,49 @@ class LinkList {
       // 找到最后的节点
       let lastNode = this._getLastNode()
       lastNode.next = newNode
-      console.log('lastNode', this.head)
     }
     this.length++
+  }
+
+  /**
+   * 根据值查找元素在链表中的位置
+   * @param {*} value
+   */
+  findIndex(value) {
+    let index = -1
+    let currentNode = this.head
+    while (currentNode) {
+      index++
+      if (currentNode.data === value) {
+        return index
+      }
+      currentNode = currentNode.next
+    }
+    return -1
+  }
+
+  /**
+   * 根据 index 删除元素
+   * @param {number} index
+   */
+  deleteAt(index) {
+    if (!this._validateIndex(index)) return null
+
+    let currentNode = this.head
+    let priviousNode = null
+    let loopIndex = 0
+
+    if (index === 0) {
+      this.head = currentNode.next
+    } else {
+      while (loopIndex++ < index) {
+        priviousNode = currentNode
+        currentNode = currentNode.next
+      }
+      priviousNode.next = currentNode.next
+    }
+    this.length--
+    return currentNode.data
   }
 
   /**
@@ -34,6 +78,7 @@ class LinkList {
       str += `,${lastNode.data}`
       lastNode = lastNode.next
     }
+
     return str.slice(1)
   }
 
@@ -57,10 +102,21 @@ class LinkList {
     }
     return currentNode
   }
+
+  _validateIndex(index) {
+    if (typeof index !== 'number' || index < 0 || index > this.length - 1) {
+      return null
+    }
+    return true
+  }
 }
 
-const linkList = new LinkList()
+const linkList = new SingleLinkList()
 linkList.append(1)
 linkList.append(3)
-console.log(linkList.length)
-console.log(linkList.toString())
+linkList.append(7)
+console.log('findIndex', linkList.findIndex(8))
+console.log('length', linkList.length)
+console.log('deleteAt', linkList.deleteAt(0))
+linkList.deleteAt(1)
+console.log('toString', linkList.toString())

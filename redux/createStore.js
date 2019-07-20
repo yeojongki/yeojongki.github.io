ddf63891465256ff09ef310942f18509aa40f94a
yeojongki <kk597@sina.com>
2019-07-20 23:50:27 +0800

    feat: redux

---
+++ redux/createStore.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * @param {Function} reducer
+ * @param {*} preloadedState
+ * @param {Function} enhancer
+ * @returns {Store}
+ */
+const createStore = (reducer, preloadedState, enhancer) => {
+  let currentState = preloadedState
+  let currentReducer = reducer
+
+  const getState = () => currentState
+
+  const currentListeners = []
+
+  const subscribe = listener => {
+    currentListeners.push(listener)
+  }
+
+  const dispatch = action => {
+    currentState = currentReducer(currentState, action)
+    currentListeners.forEach(listener => listener())
+    return action
+  }
+
+  return {
+    getState,
+    dispatch,
+    subscribe
+    // replaceReducer
+  }
+}
+
+export default createStore

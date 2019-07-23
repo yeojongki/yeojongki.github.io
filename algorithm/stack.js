de9626c33d3e249a918ca01c86f5ea2496f7fc3c
yeojongki <kk597@sina.com>
2019-07-23 15:41:31 +0800

    feat: solve leetcode <20. 有效的括号> by `stack`

--- algorithm/stack.js
+++ algorithm/stack.js
@@ -62,3 +62,60 @@ class Stack {
     return this._stack.length
   }
 }
+
+// https://leetcode-cn.com/problems/valid-parentheses/
+// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+// 有效字符串需满足：
+
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// 注意空字符串可被认为是有效字符串。
+
+// 示例 1:
+
+// 输入: "()"
+// 输出: true
+// 示例 2:
+
+// 输入: "()[]{}"
+// 输出: true
+// 示例 3:
+
+// 输入: "(]"
+// 输出: false
+// 示例 4:
+
+// 输入: "([)]"
+// 输出: false
+// 示例 5:
+
+// 输入: "{[]}"
+// 输出: true
+
+function isValid(str) {
+  const stack = new Stack()
+  let map = {
+    '(': -1,
+    ')': 1,
+    '{': -2,
+    '}': 2,
+    '[': -3,
+    ']': 3
+  }
+  for (let i = 0; i < str.length; i++) {
+    let s = str[i]
+    if (map[s] < 0) {
+      stack.push(s)
+    } else {
+      if (map[stack.pop()] + map[s] !== 0) return false
+    }
+  }
+  return true
+}
+
+console.log(isValid('()'))
+console.log(isValid('()[]{}'))
+console.log(isValid('(]'))
+console.log(isValid('([)]'))
+console.log(isValid('{[]}'))

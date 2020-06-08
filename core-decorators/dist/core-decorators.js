3517c989207134e620bb2d779c401927a628677a
yeojongki <kk597@sina.com>
2020-06-08 18:11:59 +0800

    feat(decorator): lazy initialize param

--- core-decorators/dist/core-decorators.js
+++ core-decorators/dist/core-decorators.js
@@ -1,57 +1,4 @@
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
-
-function _defineProperties(target, props) {
-  for (var i = 0; i < props.length; i++) {
-    var descriptor = props[i];
-    descriptor.enumerable = descriptor.enumerable || false;
-    descriptor.configurable = true;
-    if ("value" in descriptor) descriptor.writable = true;
-    Object.defineProperty(target, descriptor.key, descriptor);
-  }
-}
-
-function _createClass(Constructor, protoProps, staticProps) {
-  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-  if (staticProps) _defineProperties(Constructor, staticProps);
-  return Constructor;
-}
-
-function autobindClsx(clsx) {
-  var descriptors = Object.getOwnPropertyDescriptors(clsx.prototype);
-  var names = Object.getOwnPropertyNames(clsx.prototype);
-  names.forEach(function (method) {
-    var desc = descriptors[method];
-
-    if (method !== 'constructor') {
-      Object.defineProperty(clsx.prototype, method, autobindMethod(clsx.prototype, method, desc));
-    }
-  });
-}
-function autobindMethod(target, method, discriptor) {
-  var fn = discriptor.value;
-  return {
-    get: function get() {
-      var boundFn = fn.bind(this);
-      Object.defineProperty(this, method, {
-        value: boundFn
-      });
-      return boundFn;
-    }
-  };
-}
-function autobind() {
-  if (arguments.length === 1) {
-    return autobindClsx(arguments.length <= 0 ? undefined : arguments[0]);
-  } else {
-    return autobindMethod.apply(void 0, arguments);
-  }
-}
-
-var _class;
+// import { autobind } from 'core-decorators'
 // class Person {
 //   @memoize("anotherMethod")
 //   doBigCalcFn() {
@@ -66,41 +13,36 @@ var _class;
 // console.log(doBigCalcFn())
 
 /*******************************************************************/
-// 2. autobind (class / method)
+// // 2. autobind (class / method)
+// @autobind
+// class Person {
+//   // @autobind
+//   getInstance() {
+//     return this
+//   }
+//   anotherGetInstance() {
+//     return this
+//   }
+// }
+// const person = new Person()
+// const { getInstance, anotherGetInstance } = person
+// const p1 = getInstance()
+// const p2 = anotherGetInstance()
+// console.log(person === p1) // true
+// console.log(person === p2) // true
+// console.log(p1 === p2) // true
+// console.log(person === p1 === p2) // false why?
 
-var Person = autobind(_class = /*#__PURE__*/function () {
-  function Person() {
-    _classCallCheck(this, Person);
-  }
+/*******************************************************************/
+// // 3. lazy initialize
+// class Person {
+//   @lazy
+//   name = "jay"
+// }
+// const person = new Person()
+// console.log(person.name)
 
-  _createClass(Person, [{
-    key: "getInstance",
-    // @autobind
-    value: function getInstance() {
-      return this;
-    }
-  }, {
-    key: "anotherGetInstance",
-    value: function anotherGetInstance() {
-      return this;
-    }
-  }]);
+var Person$1 = Person;
 
-  return Person;
-}()) || _class;
-
-var person = new Person();
-var getInstance = person.getInstance,
-    anotherGetInstance = person.anotherGetInstance;
-var p1 = getInstance();
-var p2 = anotherGetInstance();
-console.log(person === p1); // true
-
-console.log(person === p2); // true
-
-console.log(p1 === p2); // true
-
-console.log(person === p1 === p2); // false why?
-
-export default Person;
+export default Person$1;
 //# sourceMappingURL=core-decorators.js.map
--- core-decorators/src/index.js
+++ core-decorators/src/index.js
@@ -1,6 +1,7 @@
 // import { autobind } from 'core-decorators'
 import autobind from './autobind'
 import memoize from './memoize'
+import lazy from './lazy'
 
 // // 1. memoize
 // class Person {
@@ -20,26 +21,37 @@ import memoize from './memoize'
 
 /*******************************************************************/
 
-// 2. autobind (class / method)
-@autobind
-class Person {
-  // @autobind
-  getInstance() {
-    return this
-  }
+// // 2. autobind (class / method)
+// @autobind
+// class Person {
+//   // @autobind
+//   getInstance() {
+//     return this
+//   }
 
-  anotherGetInstance() {
-    return this
-  }
-}
+//   anotherGetInstance() {
+//     return this
+//   }
+// }
 
-const person = new Person()
-const { getInstance, anotherGetInstance } = person
-const p1 = getInstance()
-const p2 = anotherGetInstance()
-console.log(person === p1) // true
-console.log(person === p2) // true
-console.log(p1 === p2) // true
-console.log(person === p1 === p2) // false why?
+// const person = new Person()
+// const { getInstance, anotherGetInstance } = person
+// const p1 = getInstance()
+// const p2 = anotherGetInstance()
+// console.log(person === p1) // true
+// console.log(person === p2) // true
+// console.log(p1 === p2) // true
+// console.log(person === p1 === p2) // false why?
+
+/*******************************************************************/
+
+// // 3. lazy initialize
+// class Person {
+//   @lazy
+//   name = "jay"
+// }
+
+// const person = new Person()
+// console.log(person.name)
 
 export default Person
